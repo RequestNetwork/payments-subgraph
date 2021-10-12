8a9062b4c690f00a5f1413031faa8f70ce4e9e27
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2021-10-12 11:17:43 +0200

    monitor

---
+++ scripts/monitor.ts
@@ -0,0 +1,46 @@
+import { ethers } from "ethers";
+import { request, gql } from "graphql-request";
+
+const query = gql`
+  {
+    _meta {
+      block {
+        number
+      }
+    }
+  }
+`;
+
+const providers: Record<string, string> = {
+  matic: "https://rpc-mainnet.matic.network",
+  celo: "https://forno.celo.org"
+};
+
+const getNetworkStatus = async (network: string) => {
+  const data = await request(
+    `https://api.thegraph.com/subgraphs/name/requestnetwork/request-payments-${network}`,
+    query
+  );
+  const lastSyncedBlock = data._meta.block.number;
+
+  const provider = providers[network]
+    ? new ethers.providers.JsonRpcProvider(providers[network])
+    : ethers.getDefaultProvider(network);
+  const lastBlock = await provider.getBlockNumber();
+
+  return {
+    network,
+    lastBlock,
+    lastSyncedBlock,
+    delay: lastBlock - lastSyncedBlock
+  };
+};
+
+const main = async () => {
+  const statuses = await Promise.all(
+    ["rinkeby", "matic", "celo"].map(getNetworkStatus)
+  );
+  console.table(statuses);
+};
+
+main();
