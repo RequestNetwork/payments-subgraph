ddbab47ff32d06f1ec3ed15cedac908f6070e17d
darko <kolevdarko@gmail.com>
2021-11-03 12:41:48 +0100

    ethFeeProxy event handler handleTransferWithReferenceAndFee

--- src/ethFeeProxy.ts
+++ src/ethFeeProxy.ts
@@ -1,26 +1,34 @@
 import { log } from "@graphprotocol/graph-ts";
-import { TransferWithReference } from "../generated/EthProxy/EthProxy";
+import { TransferWithReferenceAndFee } from "../generated/EthFeeProxy/EthFeeProxy"
 import { Payment } from "../generated/schema";
 import { generateId } from "./shared";
 
-export function handleTransferWithReference(
-  event: TransferWithReference
+export function createPaymentForFeeProxy(
+    event: TransferWithReferenceAndFee
+): Payment {
+  let payment = new Payment(
+      generateId(event.transaction, event.params.paymentReference)
+  );
+  payment.contractAddress = event.address;
+  payment.reference = event.params.paymentReference;
+  payment.to = event.params.to;
+  payment.from = event.transaction.from;
+  payment.block = event.block.number.toI32();
+  payment.timestamp = event.block.timestamp.toI32();
+  payment.txHash = event.transaction.hash;
+  payment.gasUsed = event.transaction.gasUsed;
+  payment.gasPrice = event.transaction.gasPrice;
+  payment.amount = event.params.amount.toBigDecimal();
+  payment.feeAmount = event.params.feeAmount.toBigDecimal();
+  payment.feeAddress = event.params.feeAddress;
+
+  return payment;
+}
+
+export function handleTransferWithReferenceAndFee(
+  event: TransferWithReferenceAndFee
 ): void {
   log.info("ethProxy for tx {}", [event.transaction.hash.toHexString()]);
-  let payment = new Payment(
-    generateId(event.transaction, event.params.paymentReference)
-  );
-  // TODO
-  // payment.contractAddress = event.address;
-  // payment.reference = event.params.paymentReference;
-  // payment.to = event.params.to;
-  // payment.from = event.transaction.from;
-  // payment.block = event.block.number.toI32();
-  // payment.timestamp = event.block.timestamp.toI32();
-  // payment.txHash = event.transaction.hash;
-  // payment.gasUsed = event.transaction.gasUsed;
-  // payment.gasPrice = event.transaction.gasPrice;
-  // payment.amount = event.params.amount.toBigDecimal();
-
+  let payment = createPaymentForFeeProxy(event);
   payment.save();
 }
