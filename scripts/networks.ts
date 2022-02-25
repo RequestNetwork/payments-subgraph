0b3a2c844e269678033c635a7fbeaa467c8cc662
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2022-02-25 08:51:13 +0200

    chore: adding missing networks to scripts

--- scripts/networks.ts
+++ scripts/networks.ts
@@ -1,10 +1,12 @@
 export const providers: Record<string, string> = {
   mainnet: "",
   rinkeby: "",
+  xdai: "https://rpc.gnosischain.com/",
   fuse: "https://rpc.fuse.io/",
   matic: "https://rpc-mainnet.maticvigil.com",
   celo: "https://forno.celo.org",
   fantom: "https://rpc.ftm.tools",
+  bsc: "https://bsc-dataseed.binance.org/",
   "arbitrum-rinkeby": "https://rinkeby.arbitrum.io/rpc",
   "arbitrum-one": "https://arb1.arbitrum.io/rpc",
 };
