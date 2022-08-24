83814e69c257a53569a00328b573d0b660bb4e03
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2022-08-24 15:13:29 +0200

    chore: fix configure-ci
    [skip ci]

--- cli/commands/configure-ci.ts
+++ cli/commands/configure-ci.ts
@@ -3,13 +3,15 @@ import networks from "../networks.json";
 import { exec } from "child_process";
 
 export const command = "configure-ci [network..]";
-export const desc = "Deploys one subgraph";
+export const desc =
+  "Configures Github Environments for CI. Configuration is copied from the mainnet environment.";
 export const builder = (y: yargs.Argv) =>
   y.positional("network", {
     desc: "The network to deploy to",
     type: "string",
     array: true,
     choices: networks.filter(x => x !== "mainnet"),
+    demandOption: true,
   });
 
 const gh = async <T = any>(
@@ -46,28 +48,29 @@ const gh = async <T = any>(
 };
 
 export const handler = async ({
-  network,
+  network: networkList,
 }: Awaited<ReturnType<typeof builder>["argv"]>) => {
-  // Get the mainnet configuration
-  const mainnet = await gh<{
-    protection_rules: {
-      reviewers: { type: string; reviewer: { id: number } }[];
-    }[];
-    deployment_branch_policy: any;
-  }>("repos/:owner/:repo/environments/mainnet");
+  for (const network of networkList) {
+    // Get the mainnet configuration
+    const mainnet = await gh<{
+      protection_rules: {
+        reviewers: { type: string; reviewer: { id: number } }[];
+      }[];
+      deployment_branch_policy: any;
+    }>("repos/:owner/:repo/environments/mainnet");
 
-  // format the reviewers field
-  const reviewers = mainnet.protection_rules[0]?.reviewers.map(
-    ({ type, reviewer }) => ({ type, id: reviewer.id }),
-  );
+    // format the reviewers field
+    const reviewers = mainnet.protection_rules[0]?.reviewers.map(
+      ({ type, reviewer }) => ({ type, id: reviewer.id }),
+    );
 
-  // create or update the environment based on mainnet values
-  const r = await gh(`repos/:owner/:repo/environments/${network}`, {
-    method: "PUT",
-    input: {
-      reviewers,
-      deployment_branch_policy: mainnet.deployment_branch_policy,
-    },
-  });
-  console.log(r);
+    // create or update the environment based on mainnet values
+    await gh(`repos/:owner/:repo/environments/${network}`, {
+      method: "PUT",
+      input: {
+        reviewers,
+        deployment_branch_policy: mainnet.deployment_branch_policy,
+      },
+    });
+  }
 };
