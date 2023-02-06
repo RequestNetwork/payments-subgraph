4f01fbeafcb3b61c6bfd8b2b9b87392f1b60d155
Michael Liu <michael.liu95@gmail.com>
2023-02-06 15:14:06 -0500

    Updated and rebased

--- src/erc20TransferrableReceivable.ts
+++ src/erc20TransferrableReceivable.ts
@@ -1,39 +1,50 @@
 import { ethereum, log } from "@graphprotocol/graph-ts";
+import { TransferWithReferenceAndFee } from "../generated/ERC20FeeProxy/ERC20FeeProxy";
 import { Payment as PaymentEvent } from "../generated/ERC20TransferrableReceivable/ERC20TransferrableReceivable";
-import { ReceivablePayment } from "../generated/schema";
+import { Payment } from "../generated/schema";
+import { createPaymentForFeeProxy } from "./erc20FeeProxy";
 import { generateId } from "./shared";
 
-export function createReceivablePaymentForTransferrableReceivable(
-  event: PaymentEvent,
-): ReceivablePayment {
-  let receivablePayment = new ReceivablePayment(
-    generateId(event.transaction, event.params.paymentReference),
-  );
+//
+// ERC20TransferrableReceivable calls ERC20FeeProxy internally.
+// Hence, we need to first parse the ERC20FeeProxy event (TransferWithReferenceAndFee), then
+// parse the Payment event
+//
 
-  receivablePayment.contractAddress = event.address;
-  receivablePayment.reference = event.params.paymentReference;
-  receivablePayment.tokenAddress = event.params.assetAddress;
-  receivablePayment.paymentProxy = event.params.paymentProxy;
-  receivablePayment.from = event.transaction.from;
-  receivablePayment.to = event.params.recipient;
-  receivablePayment.block = event.block.number.toI32();
-  receivablePayment.timestamp = event.block.timestamp.toI32();
-  receivablePayment.txHash = event.transaction.hash;
-  receivablePayment.gasUsed = (event.receipt as ethereum.TransactionReceipt).gasUsed;
-  receivablePayment.gasPrice = event.transaction.gasPrice;
-  receivablePayment.amount = event.params.amount.toBigDecimal();
-  receivablePayment.feeAmount = event.params.feeAmount.toBigDecimal();
-  receivablePayment.feeAddress = event.params.feeAddress;
+/**
+ * Handle the TransferWithReferenceAndFee event, emitted by the external call to the ERC20FeeProxy contract.
+ * @param event
+ */
+export function handleTransferWithReferenceAndFee(
+  event: TransferWithReferenceAndFee,
+): void {
+  log.info("feeProxy (transferrable receivable) for tx {}", [
+    event.transaction.hash.toHexString(),
+  ]);
+  log.info("feeProxy address {}", [event.address.toHexString()]);
+  let payment = createPaymentForFeeProxy(event);
 
-  return receivablePayment;
+  payment.save();
 }
 
 export function handleReceivablePayment(event: PaymentEvent): void {
   log.info("receivable payment event for tx {}", [
     event.transaction.hash.toHexString(),
   ]);
-  let receivablePayment = createReceivablePaymentForTransferrableReceivable(
-    event,
-  );
-  receivablePayment.save();
+  log.info("receivable address {}", [event.address.toHexString()]);
+  let id = generateId(event.transaction, event.params.paymentReference);
+  let payment = Payment.load(id);
+
+  if (!payment) {
+    log.error("payment entity {} should already exist. (tx: {})", [
+      id,
+      event.transaction.hash.toHexString(),
+    ]);
+    return;
+  }
+
+  // override contractAddress and amount, already set by previous handler
+  payment.contractAddress = event.address;
+
+  payment.save();
 }
