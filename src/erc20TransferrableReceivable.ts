4d15c17a284d640bc2d71d1935b48d3401784ec6
Michael Liu <michael.liu95@gmail.com>
2023-02-10 15:07:46 -0500

    Update contract abi and subgraph configs

--- src/erc20TransferrableReceivable.ts
+++ src/erc20TransferrableReceivable.ts
@@ -1,9 +1,6 @@
-import { ethereum, log } from "@graphprotocol/graph-ts";
+import { log } from "@graphprotocol/graph-ts";
 import { TransferWithReferenceAndFee } from "../generated/ERC20FeeProxy/ERC20FeeProxy";
-import { Payment as PaymentEvent } from "../generated/ERC20TransferrableReceivable/ERC20TransferrableReceivable";
-import { Payment } from "../generated/schema";
 import { createPaymentForFeeProxy } from "./erc20FeeProxy";
-import { generateId } from "./shared";
 
 //
 // ERC20TransferrableReceivable calls ERC20FeeProxy internally.
@@ -21,30 +18,7 @@ export function handleTransferWithReferenceAndFee(
   log.info("feeProxy (transferrable receivable) for tx {}", [
     event.transaction.hash.toHexString(),
   ]);
-  log.info("feeProxy address {}", [event.address.toHexString()]);
   let payment = createPaymentForFeeProxy(event);
-
-  payment.save();
-}
-
-export function handleReceivablePayment(event: PaymentEvent): void {
-  log.info("receivable payment event for tx {}", [
-    event.transaction.hash.toHexString(),
-  ]);
-  log.info("receivable address {}", [event.address.toHexString()]);
-  let id = generateId(event.transaction, event.params.paymentReference);
-  let payment = Payment.load(id);
-
-  if (!payment) {
-    log.error("payment entity {} should already exist. (tx: {})", [
-      id,
-      event.transaction.hash.toHexString(),
-    ]);
-    return;
-  }
-
-  // override contractAddress and amount, already set by previous handler
-  payment.contractAddress = event.address;
 
   payment.save();
 }
