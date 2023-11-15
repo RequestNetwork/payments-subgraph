8c12e110b0af3392c481f8913d885769092fbfb3
leoslr <50319677+leoslr@users.noreply.github.com>
2023-11-15 15:05:14 +0100

    feat: core subgraph deployment (#77)
    Co-authored-by: Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>

--- cli/graph-nodes.ts
+++ cli/graph-nodes.ts
@@ -1,37 +1,53 @@
 type GraphNodeInfo = {
-    queryBase: string
-    deploy: string
-    index: string
-    ipfs: string
-}
+  queryBase: string;
+  deploy: string;
+  index: string;
+  ipfs: string;
+};
 
-type GraphNodeInfoByNetwork = Record<string, GraphNodeInfo>
+type GraphNodeInfoByNetwork = Record<string, GraphNodeInfo>;
 
 export const graphNodeInfoByNetwork: GraphNodeInfoByNetwork = {
-    /* Graph Node maintained by Mantle Foundation:
-    https://docs.mantle.xyz/network/for-devs/resources-and-tooling/graph-endpoints */
-    "mantle-testnet": {
-        queryBase: "https://graph.testnet.mantle.xyz",
-        deploy: "https://graph.testnet.mantle.xyz/deploy",
-        index: "https://graph.testnet.mantle.xyz/graphql",
-        ipfs: "https://ipfs.testnet.mantle.xyz",
-    },
-    // Graph Node maintained by FusionX: https://fusionx.finance/
-    "mantle": {
-        queryBase: "https://graph.fusionx.finance",
-        // FusionX Graph Node implements deploy endpoint as a subdomain
-        deploy: "https://deploy.graph.fusionx.finance",
-        index: "https://graph.fusionx.finance/graphql",
-        ipfs: "https://api.thegraph.com/ipfs",
-    }
-}
+  /**
+   * Graph Node maintained by Mantle Foundation:
+   * https://docs.mantle.xyz/network/for-devs/resources-and-tooling/graph-endpoints
+   */
+  "mantle-testnet": {
+    queryBase: "https://graph.testnet.mantle.xyz",
+    deploy: "https://graph.testnet.mantle.xyz/deploy",
+    index: "https://graph.testnet.mantle.xyz/graphql",
+    ipfs: "https://ipfs.testnet.mantle.xyz",
+  },
+  /**
+   * Graph Node maintained by FusionX
+   * https://fusionx.finance/
+   */
+  mantle: {
+    queryBase: "https://graph.fusionx.finance",
+    // FusionX Graph Node implements deploy endpoint as a subdomain
+    deploy: "https://deploy.graph.fusionx.finance",
+    index: "https://graph.fusionx.finance/graphql",
+    ipfs: "https://api.thegraph.com/ipfs",
+  },
+  /**
+   * Graph Node maintained by CORE DAO:
+   * https://docs.coredao.org/developer/develop-on-core/core-subgraph
+   */
+  core: {
+    queryBase: "https://thegraph.coredao.org",
+    // FusionX Graph Node implements deploy endpoint as a subdomain
+    deploy: "https://thegraph.coredao.org/deploy/",
+    index: "https://thegraph.coredao.org/graphql",
+    ipfs: "https://thegraph.coredao.org/ipfs/",
+  },
+};
 
 // TheGraph Hosted Service
 export const defaultGraphNodeInfo: GraphNodeInfo = {
-    queryBase: "https://api.thegraph.com",
-    deploy: "https://api.thegraph.com/deploy",
-    /* TheGraph Hosted Service has a different endpoint for checking subgraph health.
+  queryBase: "https://api.thegraph.com",
+  deploy: "https://api.thegraph.com/deploy",
+  /* TheGraph Hosted Service has a different endpoint for checking subgraph health.
     https://thegraph.com/docs/en/deploying/deploying-a-subgraph-to-hosted/#checking-subgraph-health */
-    index: "https://api.thegraph.com/index-node/graphql",
-    ipfs: "https://api.thegraph.com/ipfs",
-}
+  index: "https://api.thegraph.com/index-node/graphql",
+  ipfs: "https://api.thegraph.com/ipfs",
+};
