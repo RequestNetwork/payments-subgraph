1d6ee60334971da128f1dd45d29a9db6b03e5414
Aimen Sahnoun <aimen@aviatolabs.xyz>
2024-11-11 18:50:55 +0300

    feat: improve single request proxy (#107)

--- src/singleRequestProxyFactory.ts
+++ src/singleRequestProxyFactory.ts
@@ -8,11 +8,14 @@ export function handleEthereumSingleRequestProxyCreated(
   event: EthereumSingleRequestProxyCreated,
 ): void {
   let deployment = new SingleRequestProxyDeployment(
-    event.transaction.hash.toHexString(),
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString(),
   );
   deployment.proxyAddress = event.params.proxyAddress;
   deployment.payee = event.params.payee;
   deployment.paymentReference = event.params.paymentReference;
+  deployment.feeAddress = event.params.feeAddress;
+  deployment.feeAmount = event.params.feeAmount;
+  deployment.feeProxyUsed = event.params.feeProxyUsed;
   deployment.proxyType = "Ethereum";
   deployment.block = event.block.number.toI32();
   deployment.timestamp = event.block.timestamp.toI32();
@@ -24,12 +27,15 @@ export function handleERC20SingleRequestProxyCreated(
   event: ERC20SingleRequestProxyCreated,
 ): void {
   let deployment = new SingleRequestProxyDeployment(
-    event.transaction.hash.toHexString(),
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString(),
   );
   deployment.proxyAddress = event.params.proxyAddress;
   deployment.payee = event.params.payee;
   deployment.tokenAddress = event.params.tokenAddress;
   deployment.paymentReference = event.params.paymentReference;
+  deployment.feeAddress = event.params.feeAddress;
+  deployment.feeAmount = event.params.feeAmount;
+  deployment.feeProxyUsed = event.params.feeProxyUsed;
   deployment.proxyType = "ERC20";
   deployment.block = event.block.number.toI32();
   deployment.timestamp = event.block.timestamp.toI32();
